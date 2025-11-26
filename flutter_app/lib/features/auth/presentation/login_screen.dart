3733465cd5afbc01b163d261fd431b76285858da
leosaracino <leonardosaracino22@gmail.com>
2025-11-26 10:54:47 -0300

    Tirando necessidade de login

--- flutter_app/lib/features/auth/presentation/login_screen.dart
+++ flutter_app/lib/features/auth/presentation/login_screen.dart
@@ -24,8 +24,10 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
+  // final _emailController = TextEditingController();
+  // final _passwordController = TextEditingController();
+  final _emailController = TextEditingController(text: "teste@gmail.com");
+  final _passwordController = TextEditingController(text: "Abcd1234");
   bool _isLoading = false;
 
   @override
